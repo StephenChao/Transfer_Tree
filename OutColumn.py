62ca740ad2fe584eba0a5243c4ab91997320d93d
YuzheZhao <60813920+StephenChao@users.noreply.github.com>
2021-12-07 17:48:29 +0800

    Add files via upload

---
+++ OutColumn.py
@@ -0,0 +1,17 @@
+from _typeshed import Self
+
+
+if self.MC:
+    
+    # ===================================== Lepton =====================================
+    self.OutColumn += [
+        "AK8_Mode","weight","H_pt","H_msd","deltaR","deltaR2","Nj8","MET_pt","MET_HT",
+            ]
+
+
+if self.Data:
+
+    self.OutColumn += [
+                "H_pt","H_msd","MET_HT",
+            ]
+            
